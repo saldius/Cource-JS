26e938af77b4686257d74d35674d0763a699d25a
Serg <saldius@gmail.com>
2018-10-07 00:28:06 +1000

    первый комит 1дз

--- Homework lesson 1/js/main.js
+++ Homework lesson 1/js/main.js
@@ -1,24 +1,26 @@
-'use strict',
+'use strict';
 
-let money= prompt('Ваш бюджет на месяц?', ''),
+var money= prompt('Ваш бюджет на месяц?', ''),
     time=prompt('Введите дату в формате YYYY-MM-DD', ''),
-    
+    timeData,
+    budget=money;
+       
 let appData = {
         budget:money,
         time:timeData,
         expenses:{},
         optionalExpenses:[],
         income:false,
-};
+    };
 let answerOn1Question=prompt('Введите обязательную статью расходов в этом месяце',''),
     answerOn2Question=prompt('Во сколько обойдется?',''),
     answerOn3Question=prompt('Введите обязательную статью расходов в этом месяце',''),
     answerOn4Question=prompt('Во сколько обойдется?','');
 
-let budget1day=budget/30;
-
-console.log(appData);
-alert ('бюджет на 1 день :', + budget1day);
+console.log(money);
+//console.log(time);
+//console.log(appData);
+alert ('Ваш бюджет на 1 день составит: ' + budget/30+' рублей');
 
 
 
