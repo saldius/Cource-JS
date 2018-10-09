1df2f9a53d8cd8f74ad2b0b7bb9dc1e2c763ae16
Serg <saldius@gmail.com>
2018-10-09 12:33:54 +1000

    8 коммит правки в дз2 цикл for

--- Homework lesson 2/js/main.js
+++ Homework lesson 2/js/main.js
@@ -14,13 +14,14 @@ let appData = {
         saving:false
     };
     
-//for (let=i=0; i<2; i++) {
+//for (let i=0; i<2; i++) {
 //     let a=prompt('Введите обязательную статью расходов в этом месяце',''),
 //         b=prompt('Во сколько обойдется?','');
 //    if ( (typeof (a))=== 'string' && (typeof (a)) !=null && (typeof (b)) !=null 
 //       && a!='' && b!=''&& a.lenght <50) {
 //        console.log("done");
-//        appData.expenses[a]=b;        
+//        appData.expenses[a]=b;
+//                    
 //    } else {
 //
 //    }
@@ -39,7 +40,7 @@ let appData = {
     appData.expenses[a] = b;
     i++;
    } else {
-    console.log("error");
+    console.log("Произошла ошибка");
     i--;
    }
   }
