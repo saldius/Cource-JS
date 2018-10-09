fc325b1097634e03a4055fa7eba9f4f04e2bf315
Serg <saldius@gmail.com>
2018-10-09 17:10:19 +1000

    error at words lenth

--- Homework lesson 2/js/main.js
+++ Homework lesson 2/js/main.js
@@ -18,7 +18,7 @@ for (let i=0; i<2; i++) {
      let a=prompt('Введите обязательную статью расходов в этом месяце',''),
          b=prompt('Во сколько обойдется?','');
              if ( (typeof (a))=== 'string' && (typeof (a)) !=null && (typeof (b)) !=null && 
-       a !='' && b!=''&& a.lenght <50) {
+       a !='' && b!=''&& a.length <50) {
         console.log("done");
         appData.expenses[a]=b;
 
@@ -55,7 +55,7 @@ do {
          b=prompt('Во сколько обойдется?','');
          i++;
     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null  &&
-         a != '' && b != '' && a.lentgh < 50 ) {
+         a != '' && b != '' && a.length < 50 ) {
          console.log("done");
          appData.expenses[a] = b;  
     } else {
