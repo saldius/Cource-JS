fed8ba99563b5582fb7cbcef98c339ee8e156c31
Serg <saldius@gmail.com>
2018-10-18 00:11:11 +1000

    правки 6 дз

--- Hopmework lesson6/js/main.js
+++ Hopmework lesson6/js/main.js
@@ -1,8 +1,8 @@
 
-let btn_start=document.getElementById('start'),
-    budgetValue=document.getElementsByClassName('budget-value')[0],
-    dayBudgetValue=document.getElementsByClassName('daybudget-value')[0],
-    levelValue=document.getElementsByClassName('level-value')[0],
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
     expensesValue = document.getElementsByClassName('expenses-value')[0],
     optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
     incomeValue = document.getElementsByClassName('income-value')[0],
@@ -14,13 +14,13 @@ let btn_start=document.getElementById('start'),
     optionalExpensesBtn = document.getElementsByTagName('button')[1],
     countBtn = document.getElementsByTagName('button')[2],
     optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
-    incomeItem= document.querySelector('.choose-income'), 
-    checkSavings= document.querySelector('#savings'),
-    sumValue= document.querySelector('.choose-sum'),
-    percentValue= document.querySelector('.choose-percent'),
-    yearValue= document.querySelector('.year-value'),
-    monthValue= document.querySelector('.month-value'),
-    dayValue= document.querySelector('.day-value');
+    incomeItem = document.querySelector('.choose-income'), 
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
 
 let money, time;
 
@@ -28,19 +28,20 @@ expensesBtn.disabled =  true;
 optionalExpensesBtn.disabled =  true; 
 countBtn.disabled =  true;
 
-startBtn=addEventListener('click', function() {
+
+startBtn.addEventListener('click', function() {
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
     money = +prompt("Ваш бюджет на месяц?", '');
 
-    while (isNaN(money) || money=="" || money==null ){
-        money = +prompt("Ваш бюджет ?", '');        
+    while (isNaN(money) || money == "" || money==null ){
+        money = +prompt("Ваш бюджет на месяц?", '');        
     }
     appData.budget = money;
     appData.time = time;
     budgetValue.textContent = money.toFixed();
-    yearValue.value = newDate(Date.parse(time)).getFullYear(),
-    monthValue.value = newDate(Date.parse(time)).getMonth() + 1,
-    dayValue.value = newDate(Date.parse(time)).getDate();
+    yearValue.value = new Date(Date.parse(time)).getFullYear();
+    monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
+    dayValue.value = new Date(Date.parse(time)).getDate();
     expensesBtn.disabled = false;
     optionalExpensesBtn.disabled = false;
     countBtn.disabled = false;
@@ -49,15 +50,17 @@ startBtn=addEventListener('click', function() {
 expensesBtn.addEventListener('click', function() {
     let sum = 0;
     for (let i=0; i<expensesItem.length; i++) {    
-        let a= expensesItem[i].value,
-            b= expensesItem[++i].value;
+        let a = expensesItem[i].value,
+            b = expensesItem[++i].value;
 
-        if ( (typeof (a))=== 'string' && (typeof (a)) !=null && (typeof (b)) !=null && 
-            a !='' && b!=''&& a.length <50) {
+     if ( (typeof(a))=== 'string' && a != null && b != null 
+        && a != '' && b != '' && a.length < 50 ) {
+
             console.log("Все верно");
-            appData.expenses[a]=b;
+            appData.expenses[a] = b;
             sum += +b;
         } else {
+            console.log("Произошла ошибка");
             i = i-1;
         }
     }  
@@ -65,8 +68,8 @@ expensesBtn.addEventListener('click', function() {
 });
 
 optionalExpensesBtn.addEventListener('click', function() {
-    for (let i = 1; j < optionalexpenses-item.length; i++) {
-        let opt= optionalexpenses-item[i].value;
+    for (let i = 1; i < optionalExpensesItem.length; i++) {
+        let opt = optionalExpensesItem[i].value;
         
         appData.optionalExpenses[i] = opt;
         optionalExpensesValue.textContent += appData.optionalExpenses[i] + ' ';
@@ -75,9 +78,14 @@ optionalExpensesBtn.addEventListener('click', function() {
 });
 
 countBtn.addEventListener('click', function() {
-    if (appData.budget  != undefined) {    
-       appData.moneyPerDay = (appData.budget / 30).toFixed(); 
-       dayBudgetValue.textContent = appData.moneyPerDay;
+    if (appData.budget  != undefined) {  
+
+        //appData.moneyPerDay = (appData.budget-sum / 30).toFixed(); 
+        //dayBudgetValue.textContent = appData.moneyPerDay;
+        //appData.moneyPerDay = ((appData.budget - expensesValue.textContent) / 30 ).toFixed();
+
+        appData.moneyPerDay = ((appData.budget - expensesValue.textContent) / 30 ).toFixed();
+        dayBudgetValue.textContent = appData.moneyPerDay ; 
 
        if (appData.moneyPerDay <100){
             levelValue.textContent = "Минимальный уровень достатка";
@@ -88,7 +96,7 @@ countBtn.addEventListener('click', function() {
         } else {
             levelValue.textContent = "Произошла ошибка";
         } 
-    } else {
+        } else {
         dayBudgetValue.textContent = "Произошла ошибка";
     }    
 });
@@ -96,16 +104,17 @@ countBtn.addEventListener('click', function() {
 incomeItem.addEventListener('input', function() {
     let items = incomeItem.value;
     
-    appData.income = items.split(', ');
-    incomeValue.textContent = appData.income;
-
+        appData.income = items.split(', ');
+        incomeValue.textContent = appData.income;    
 });
 
 checkSavings.addEventListener('click', function() {
     if (appData.saving == true) {
-        appData.saving= false;      
+        appData.saving = false;
+      
     } else {
         appData.saving = true;
+
     }
 });
 
@@ -154,4 +163,4 @@ let appData = {
 
 
 
--
+
